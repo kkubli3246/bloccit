58e80805be14cc75f35b74e76ea27c1c72270c9f
Summer Guijarro <summerguijarro@Summers-MacBook-Air.local>
2019-06-11 17:02:47 -0700

    bloccit assignment 4: complete

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -5,9 +5,15 @@ module.exports = (sequelize, DataTypes) => {
     description: DataTypes.STRING
   }, {});
   Topic.associate = function(models) {
+
     Topic.hasMany(models.Banner, {
       foreignKey: "topicId",
       as: "banners",
+    });
+
+    Topic.hasMany(models.Rule, {
+      foreignKey: "topicId",
+      as: "rules",
     });
   };
   return Topic;
